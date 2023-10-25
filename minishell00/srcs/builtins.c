0fcefc7441ab1f4eb2b223b518848e009479509a
Yabing <yabingc3@gmail.com>
2023-10-25 17:40:35 +0200

    segfault unset SHELL

--- minishell00/srcs/builtins.c
+++ minishell00/srcs/builtins.c
@@ -6,7 +6,7 @@
 /*   By: yachen <yachen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 11:02:28 by yachen            #+#    #+#             */
-/*   Updated: 2023/10/24 18:05:42 by yachen           ###   ########.fr       */
+/*   Updated: 2023/10/25 17:36:06 by yachen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,6 @@ void	ft_env(t_list *envlist)
 		printf("%s\n", (char *)tmp->content);
 		tmp = tmp->next;
 	}
-	clear_lst(&envlist);
 }
 
 int	str_cmp(char *model, char *str_tocmp)
@@ -72,7 +71,7 @@ int	str_cmp(char *model, char *str_tocmp)
 	int	i;
 
 	i = 0;
-	while (model[i])
+	while (model[i] && model[i - 1] != '=')
 	{
 		if (str_tocmp[i] != model[i])
 			return (0);
@@ -81,31 +80,87 @@ int	str_cmp(char *model, char *str_tocmp)
 	return (1);
 }
 
-void	ft_unset(t_list *envlist, char *varto_unset)
+int	find_equal(char *str, char equal)
 {
-	int		i;
-	t_list	*tmp;
-	t_list	*tmp2;
+	int	i;
 
-	if (!varto_unset)
-		return ;
 	i = 0;
-	tmp = envlist;
-	tmp2 = envlist;
-	while (tmp != NULL)
+	while (str[i])
 	{
-		if (str_cmp(varto_unset, (char *)(tmp)->content) == 1)
-		{
-			while (i-- > 0)
-				(tmp2)++;
-			(tmp2)->next = tmp->next;
-			printf("%s\n", (char *)tmp->content);
-			free(tmp);
-			return ;
-		}
-		tmp = tmp->next;
+		if (str[i] == equal)
+			return (i);
 		i++;
 	}
+	return (0);
+}
+
+t_list	*ft_unset(t_list *envlist, char *varto_unset)
+{
+	t_list	*current;
+	t_list	*tmp;
+
+	if ((!varto_unset) || (find_equal(varto_unset, '=')) > 0)
+		return (envlist);
+	current = envlist;
+	if (str_cmp(varto_unset, current->content) == 1 && current->next == NULL)
+	{
+		free(current);
+		return (NULL);
+	}
+	tmp = NULL;
+	while (current)
+	{
+		if (str_cmp(varto_unset, current->content) == 1)
+		{
+			tmp->next = current->next;
+			free(current);
+			if (tmp->content == NULL)
+				return (tmp->next);
+			return (tmp);
+		}
+		tmp = current;
+		current = current->next;
+	}
+	return (envlist);
+}
+
+int	ft_export(t_list *envlist, t_list *explist, char *exp_content)
+{
+	t_list	*new_var;
+	t_list	*current;
+	t_list	*tmp;
+	
+	if (!exp_content)
+	{
+		ft_env(explist);
+		return (0);
+	}
+	new_var = ft_lstnew(exp_content);
+	if (!new_var)
+	{
+		write(2, "export: new_var: malloc failed", 32);
+		return (1);
+	}
+	current = envlist;
+	if (find_equal(exp_content, '=') > 0)
+	{
+		while (current)
+		{
+			if(str_cmp(exp_content, current->content) == 1)
+			{
+				new_var->next = current->next;
+				tmp->next = new_var;
+				free(current);
+				break ;
+			}
+			tmp = current;
+			current = current->next;
+		}
+		if (!current)
+			ft_lstadd_back(&envlist, new_var);
+	}
+	ft_lstadd_back(&explist, new_var);
+	return (0);
 }
 
 /*char	*get_envhome(void)
@@ -118,7 +173,7 @@ void	ft_unset(t_list *envlist, char *varto_unset)
 		ft_putstr_fd("-minishell: ft_cd: HOME not set\n", 2);
 		return (NULL);
 	}
-	return (home += 5);
+	return (home += 5);1
 }
 
 char	*get_envpwd(char *oldpwd)
@@ -154,13 +209,26 @@ int	ft_cd(char *path)
 int	main(int argc, char **argv, char **env)
 {
 	t_list	*envlist;
+	t_list	*explist;
 
 	printf("teste : %d %s\n", argc, argv[0]);
 	envlist = env_to_envlist(env);
+	explist = env_to_envlist(env);
+	/*printf("print env list : \n\n");
 	ft_env(envlist);
+	printf("---------------------------------------------");
 	printf("\n");
-	ft_unset(envlist, "HOME");
-	printf("\n");
+	printf("print env with unset SHELL\n\n");
+*/	envlist = ft_unset(envlist, "SHELL");
 	ft_env(envlist);
-	return (0);
+//	printf("---------------------------------------------");
+//	printf("\n");
+/*	ft_export(envlist, explist, "HOME=valeur");
+	ft_env(envlist);
+	printf("print env list with export HOME=valeur\n");
+	printf("---------------------");
+	printf("\n");
+	printf("print env list with export ""\n");
+	ft_export(envlist, explist, NULL);
+*/	return (0);
 }
