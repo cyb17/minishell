94b7362953b61cd07641c47bbeaed7f8eed9c355
napncoffee <101419590+napncoffee@users.noreply.github.com>
2023-12-19 15:29:00 +0100

    Add files via upload

--- minishell05/srcs/parsing/init.c
+++ minishell05/srcs/parsing/init.c
@@ -6,7 +6,7 @@
 /*   By: achevala <achevala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 18:58:13 by nap               #+#    #+#             */
-/*   Updated: 2023/12/11 16:54:00 by achevala         ###   ########.fr       */
+/*   Updated: 2023/12/19 15:23:02 by achevala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	process_init(t_process *process)
 	process->list_tokens = NULL;
 	process->pid = -1;
 	process->next = NULL;
+	process->heredoc = NULL;
 }
 
 int	pars_init(t_p *p, char *line, t_all *all)
