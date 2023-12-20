bc1d86a47dcc7c37786f2e6e4e5a5300387fc541
napncoffee <101419590+napncoffee@users.noreply.github.com>
2023-12-20 20:17:12 +0100

    Add files via upload

--- minishell05/srcs/parsing/clean_words.c
+++ minishell05/srcs/parsing/clean_words.c
@@ -21,13 +21,10 @@ char	*delete_quotes(char *s)
 	cpy = NULL;
 	while (*s && s[i] != '\0' && i <= ((int)ft_strlen(s)))
 	{
-		if (i > 0 && s[i - 1] != '$' && s[i] && s [i + 1] && ((s[i] == '\''
-			&& s[i + 1] == '\'') || (s[i] == '"' && s[i + 1] == '"'))
-			&& b_q(s, i) == i && check_quotes(s, s[i], i) != i + 1)
+		if (i > 0 && s[i - 1] != '$' && to_delete(s, i) == true
+			&& check_quotes(s, s[i], i) != i + 1)
 			i = i + 2;
-		else if (i == 0 && s[i] && s [i + 1] && ((s[i] == '\''
-				&& s[i + 1] == '\'') || (s[i] == '"' && s[i + 1] == '"'))
-				&& b_q(s, i) == i)
+		else if (i == 0 && to_delete(s, i) == true)
 			i = i + 2;
 		else if (s[i] != '\0')
 		{
--- minishell05/srcs/parsing/utils_parsing3.c
+++ minishell05/srcs/parsing/utils_parsing3.c
@@ -6,7 +6,7 @@
 /*   By: achevala <achevala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 13:16:43 by achevala          #+#    #+#             */
-/*   Updated: 2023/12/19 13:45:04 by achevala         ###   ########.fr       */
+/*   Updated: 2023/12/20 20:12:24 by achevala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,61 +41,6 @@ char	*manage_words_p1(t_p *p, t_list **envlist)
 	return (cpy);
 }
 
-char	*manage_expand(t_p *p, t_list **envlist, char *cpy)
-{
-	char	*tmp;
-
-	(void)cpy;
-	if ((p->s3[p->i + 1] == '\'' && p->s3[p->i + 2] == '\'')
-		|| (p->s3[p->i + 1] == '"' && p->s3[p->i + 2] == '"'))
-	{
-		p->i = p->i + 3;
-		return (NULL);
-	}
-	if (b_q_exp2(p->s3, p->i) == p->i && (p->s3[p->i + 1] == '\'' || p->s3[p->i + 1] == '"'))
-	{
-		p->i++;
-		return (NULL);
-	}
-	if (p->s3[p->i + 1] >= '1' && p->s3[p->i + 1] <= '9')
-	{
-		p->i = p->i + 2;
-		return (NULL);
-	}
-	if ((p->s3[p->i + 1] == '$' || p->s3[p->i + 1] == '\'' || p->s3[p->i + 1] == '"'
-			|| p->s3[p->i + 1] == '\0'))
-		{
-			p->i++;
-			return (ft_strdup_part(p->s3, p->i - 1, p->i));
-		}
-	tmp = expand_value(p->s3, p->i, envlist);
-	p->i++;
-	if (tmp != NULL)
-	{
-		if (p->i >= p->len1 || p->s3[p->i] == '\0')
-			return (tmp);
-	}
-	if (is_exp_char(p->s3[p->i]) == true)
-	{
-		while (is_exp_char2(p->s3[p->i]) == true)
-			p->i++;
-		return (tmp);
-	}
-	else if (p->s3[p->i] == '0')
-	{
-		p->i++;
-		return (ft_strdup_part(p->all->argv0, 2, ft_strlen(p->all->argv0)));
-	}
-	else if (p->s3[p->i] == '?')
-	{
-		p->i++;
-		return (ft_itoa(g_signal));
-	}
-	else
-		return (ft_strdup_part(p->s3, p->i - 1, p->i));
-	return (tmp);
-}
-
 char	*manage_words_p2(t_p *p)
 {
 	char	*cpy;
@@ -110,43 +55,45 @@ char	*manage_words_p2(t_p *p)
 	return (cpy);
 }
 
-/* char 	*in_manage_words(t_p *p, t_list **envlist)
+char	*in_manage_p3(t_p *p, t_list **envlist, char **cpy, char **cpy2)
 {
-	char	*cpy;
-	char	*cpy2;
-	char	*tmp;
-
-	cpy = NULL;
-	cpy2 = NULL;
-	if (p->s3[p->i] != '$')
+	char *tmp;
+	
+	tmp = NULL;
+	while (p->i < p->len1)
 	{
-		cpy = cpychar(p->s3, p->i, cpy);
-		p->i++;
+		if (p->s3[p->i] != '$' || (((b_q_exp(p->s3, p->i) > p->i)
+					|| ((p-> i - 1 >= 0) && p->s3[p->i - 1] == '\\'))
+				&& p->s3[p->i] == '$'))
+		{
+			(*cpy) = cpychar(p->s3, p->i, (*cpy));
+			p->i++;
+		}
+		else if (p->s3[p->i] == '$')
+		{
+			tmp = manage_expand(p, envlist, (*cpy));
+			(*cpy2) = (*cpy);
+			(*cpy) = my_strjoin((*cpy2), tmp);
+			if (*cpy2)
+				free((*cpy2));
+			if (tmp != NULL)
+				free(tmp);
+		}
 	}
-	else if (p->s3[p->i] == '$')
-	{
-		tmp = manage_expand(p, envlist, cpy);
-		cpy2 = cpy;
-		cpy = my_strjoin(cpy2, tmp);
-		if (cpy2)
-			free(cpy2);
-		if (tmp != NULL)
-			free(tmp);
-	}
-} */
+	return (*cpy);
+}
 
 char	*manage_words_p3(t_p *p, t_list **envlist)
 {
 	char	*cpy;
 	char	*cpy2;
-	char	*tmp;
 
 	cpy = NULL;
 	cpy2 = NULL;
-	while (p->i < p->len1)
+	/* while (p->i < p->len1)
 	{
 		if (p->s3[p->i] != '$' || (((b_q_exp(p->s3, p->i) > p->i)
-				|| ((p-> i - 1 >= 0) && p->s3[p->i - 1] == '\\'))
+					|| ((p-> i - 1 >= 0) && p->s3[p->i - 1] == '\\'))
 				&& p->s3[p->i] == '$'))
 		{
 			cpy = cpychar(p->s3, p->i, cpy);
@@ -162,7 +109,8 @@ char	*manage_words_p3(t_p *p, t_list **envlist)
 			if (tmp != NULL)
 				free(tmp);
 		}
-	}
+	} */
+	cpy = in_manage_p3(p, envlist, &cpy, &cpy2);
 	if (cpy != NULL)
 	{
 		cpy2 = deepclean(cpy);
